e38ed25e11045e118605affe679d871d3c0523cb
tom <manager@163.com>
2019-10-15 16:39:29 +0800

    用户注册页面

---
+++ meiduo_demo/apps/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+
+class RegisterView(View):
+    # 用户注册
+
+    def get(self, request):
+        return render(request, 'register.html')
--- meiduo_demo/meiduo_demo/settings.py
+++ meiduo_demo/meiduo_demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&ftxb6u2o(1h!0=r4^0hgh)612wfxhv03#c^ibta&34*ql4a6@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','www.meiduo.site']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
+    'apps.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -53,10 +55,12 @@ ROOT_URLCONF = 'meiduo_demo.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.jinja2.Jinja2。',
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
+            'environment': 'utils.jinja2.environment',#默认配置
+
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -64,7 +68,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
 
             ],
-        'environment': 'utils.jinja2_env.jinja2_environment'
+
         },
     },
 ]
