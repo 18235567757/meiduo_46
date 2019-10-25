74a50bc34dbc57cbfafc2df6d0941594cf59707e
tom <manager@163.com>
2019-10-25 20:11:00 +0800

    浏览记录BUG的解决

--- meiduo_demo/apps/goods/urls.py
+++ meiduo_demo/apps/goods/urls.py
@@ -5,6 +5,7 @@ from apps.goods import views
 urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(), name='list'),
     url(r'^hot/(?P<category_id>\d+)/$', views.HotSKUView.as_view(), name='hot'),
-    url(r'^detail/(?P<sku_id>\d+)/$',views.DetailVisitView.as_view(), name='visit'),
+    url(r'^detail/visit/(?P<category_id>\d+)/$',views.DetailVisitView.as_view(), name='visitcount'),
+    url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view(), name='detail'),
 
 ]
