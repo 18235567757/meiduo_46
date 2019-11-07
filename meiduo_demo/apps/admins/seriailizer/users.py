8b95cd735e17228f91e0332a4dbde4956dd090c1
18235567757 <1127460515@qq.com>
2019-11-07 12:29:03 +0800

    添加用户功能

--- meiduo_demo/apps/admins/seriailizer/users.py
+++ meiduo_demo/apps/admins/seriailizer/users.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from apps.users.models import User
-
+import re
 
 class UserSerializers(serializers.ModelSerializer):
 
@@ -8,4 +8,29 @@ class UserSerializers(serializers.ModelSerializer):
 
         model = User
 
-        fields = ('id', 'username', 'mobile', 'email')
+        fields = ('id', 'username', 'mobile', 'email', 'password')
+
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'max_length': 20,
+                'min_length': 8,
+
+            },
+            'username': {
+                'max_length': 20,
+                'min_length': 5,
+            }
+        }
+
+    def validate_mobile(self, attrs):
+        """
+        # 验证手机号
+        :param attrs:
+        :return:
+        """
+        if not re.match(r'^1[3-9]\d{9}', attrs):
+            raise serializers.ValidationError('手机号不正确')
+
+        return attrs
+
