ded902d7ca307c144186285961795131eddfc6c5
18235567757 <1127460515@qq.com>
2019-11-08 11:15:41 +0800

    图片数据更新方法实现

--- meiduo_demo/apps/admins/seriailizer/images.py
+++ meiduo_demo/apps/admins/seriailizer/images.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from apps.goods.models import SKUImage, SKU
+from fdfs_client.client import Fdfs_client
 
 
 class SKUImageSerializer(serializers.ModelSerializer):
@@ -11,6 +12,45 @@ class SKUImageSerializer(serializers.ModelSerializer):
 
         fields = '__all__'
 
+    def create(self, validated_data):
+        # 获取前端sku对象
+        sku = validated_data['sku']
+        # 获取前端传递的图片数据
+        image = validated_data['image']
+        # 链接fastDSF
+        client = Fdfs_client('/home/python/Desktop/meiduo_46/meiduo_demo/utils/fastdfs/client.conf')
+        # 上传图片
+        ret = client.upload_by_buffer(image.read())
+        # 判断是否成功
+        if ret['Status'] != 'Upload successed.':
+            raise serializers.ValidationError('上传失败')
+        # 提取图片链接地址
+        path = ret.get('Remote file_id')
+        # 保存图片表
+        img = SKUImage.objects.create(sku=sku, image=path)
+
+        return img
+
+    def update(self, instance, validated_data):
+        # 获取前端sku对象
+        sku = validated_data['sku']
+        # 获取前端传递的图片数据
+        image = validated_data['image']
+        # 链接fastDSF
+        client = Fdfs_client('/home/python/Desktop/meiduo_46/meiduo_demo/utils/fastdfs/client.conf')
+        # 上传图片
+        ret = client.upload_by_buffer(image.read())
+        # 判断是否成功
+        if ret['Status'] != 'Upload successed.':
+            raise serializers.ValidationError('上传失败')
+        # 提取图片链接地址
+        path = ret.get('Remote file_id')
+        # 更新图片表
+
+        instance.image = path
+        instance.save()
+
+        return instance
 
 class SKUSerializer(serializers.ModelSerializer):
     # 关联返回
